5db2275ba8ff929ba73479224aec9be1bd518bca
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-04-11 12:16:19 -0400

    load CLP, token vault A, and token vault B

--- src/clmm_vault.rs
+++ src/clmm_vault.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use anchor_lang::prelude::*;
+use anchor_spl::token::TokenAccount;
 use clmm_bindings::ClpVault;
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::pubkey::Pubkey;
@@ -11,4 +12,29 @@ pub async fn get_clmm_vault(client: Arc<RpcClient>, clmm_address: Pubkey) -> Clp
   // Deserialize the account data
   let mut data: &[u8] = &clmm_vault_acct.data;
   AccountDeserialize::try_deserialize(&mut data).expect("deserialized properly")
+}
+
+pub async fn load_token_a_token_b_aum(client: Arc<RpcClient>, clmm: &ClpVault) {
+  let keys = [clmm.clp, clmm.token_vault_a, clmm.token_vault_b];
+  // Load the CLMM Vault's token accounts and whirlpool
+  let res = client.get_multiple_accounts(&keys).await.expect("Loaded CLP and token vaults OK");
+  for (index, acct) in res.iter().enumerate() {
+    if index == 0 {
+      let mut data: &[u8] = &acct.as_ref().expect("Whirlpool exists").data;
+      let clp: crate::whirlpool::Whirlpool = AccountDeserialize::try_deserialize(&mut data).expect("Whirlpool deserialized properly");
+      println!("CLP {}", clp.token_mint_a);
+    } else if index == 1 {
+      let mut data: &[u8] = &acct.as_ref().expect("Token vault a exists").data;
+      let token_vault_a: TokenAccount  = AccountDeserialize::try_deserialize(&mut data).expect("Token vault a deserialized properly");
+      println!("Token Vault A: {}", token_vault_a.amount);
+    } else if index == 2 {
+      let mut data: &[u8] = &acct.as_ref().expect("Token vault b exists").data;
+      let token_vault_a: TokenAccount  = AccountDeserialize::try_deserialize(&mut data).expect("Token vault a deserialized properly");
+      println!("Token Vault B: {}", token_vault_a.amount);
+    }
+  }
+
+  // TODO: Deserialize the token vaults and whirlpool.
+  // TODO: Load CLMM whirlpool Positions
+  // TODO: Calculate the tokenA and tokenB value of the positions
 }
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ use std::{str::FromStr, sync::Arc};
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
 
+use crate::clmm_vault::load_token_a_token_b_aum;
+
 mod clmm_vault;
+mod whirlpool;
+
+anchor_lang::declare_id!("ArmN3Av2boBg8pkkeCK9UuCN9zSUVc2UQg1qR2sKwm8d");
 
 #[tokio::main]
 async fn main() {
     let client = Arc::new(RpcClient::new_with_commitment(String::from("***REMOVED***"), CommitmentConfig::confirmed()));
     println!("Hello, world!");
 
-    // TODO: Load the CLMM Vault
-    let clmm_vault = crate::clmm_vault::get_clmm_vault(client, Pubkey::from_str("7c75jrcMMJVEPtr1hwdBQTMJCKpjquGQk9b3p237vYyc").unwrap()).await;
+    // Load the CLMM Vault
+    let clmm_vault = crate::clmm_vault::get_clmm_vault(client.clone(), Pubkey::from_str("7c75jrcMMJVEPtr1hwdBQTMJCKpjquGQk9b3p237vYyc").unwrap()).await;
     println!("mintA: {} mintB: {}", clmm_vault.token_mint_a, clmm_vault.token_mint_b);
-    // TODO: Load the CLMM Vault's token accounts
+    load_token_a_token_b_aum(client.clone(), &clmm_vault).await;
     // TODO: Load the LP mint data
     // TODO: Load all LP token hodlers and their LP balances
-    // TODO: Load CLMM whirlpool
-    // TODO: Load CLMM whirlpool Positions
-    // TODO: Calculate the tokenA and tokenB value of the positions
 }
